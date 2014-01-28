46e02535034aac3466bdf9f2f16762093a7cf227
Ganesh Iyer <lastlegion@gmail.com>
2014-01-29 00:25:26 +0530

    Using native cv2 sobel operators

--- HOG.py
+++ HOG.py
@@ -1,16 +1,19 @@
 #pyHog
 from SimpleCV import *
-def HOG(Img, no_divs, no_bins):
+def HOG(Img, no_divs=3, no_bins=6):
     
     n_HOG = no_divs*no_divs*no_bins;
     
     #Initialize output HOG vector
-    HOG = [0]*n_HOG    
+    HOG = [0.0]*n_HOG    
 
     #Apply sobel on image to find x and y orientations of the image
-    Ix = Img.sobel(1,0, True, 3)
-    Iy = Img.sobel(0,1,True, 3)
-    
+    Icv = Img.getNumpyCv2()
+    Ix = cv2.Sobel(Icv, ddepth = cv.CV_32F, dx=1, dy=0, ksize=3)
+    Iy = cv2.Sobel(Icv, ddepth = cv.CV_32F, dx=0, dy=1, ksize=3)
+ 
+    Ix = Ix.transpose(1,0,2)
+    Iy = Iy.transpose(1,0,2)
     cellx = Img.width/no_divs     #width of each cell(division)
     celly = Img.height/no_divs    #height of each cell(division)    
     
@@ -18,9 +21,12 @@ def HOG(Img, no_divs, no_bins):
     img_area = Img.height * Img.width
 
     #Range of each bin
-    BIN_RANGE = 2*pi/no_bins
+    BIN_RANGE = (2*pi)/(no_bins)
 
     m=0
+    print Img.size()
+    print Ix.shape
+    
     while m < no_divs:
         n = 0
         while n < no_divs:
@@ -28,6 +34,8 @@ def HOG(Img, no_divs, no_bins):
             while i < cellx:
                 j=0
                 while j < celly:
+                    print m*cellx+i
+                    print n*celly+j
                     px = Ix[m*cellx +i, n*celly+j][0]
                     py = Iy[m*cellx+i, n*celly+j][0]
                     
@@ -35,15 +43,14 @@ def HOG(Img, no_divs, no_bins):
                     grad = sqrt(px*px + py*py)
                     #normalized grad value
                     norm_grad = grad/img_area
-
+                    #print norm_grad
                     #Angle
                     angle = atan2(py,px)
-                    
-                    if(angle< 0):
-                        angle+= 2*pi
-
-                    nth_bin = angle/BIN_RANGE
-
+                    if(angle < 0):
+                        angle = angle+ 2*pi
+                    nth_bin = floor(float(angle/BIN_RANGE))
+                    #nth_bin = angle*(180/pi)%180
+                    #print nth_bin
                     HOG[((m*no_divs+n)*no_bins + int(nth_bin))] += norm_grad
                     j=j+1
                 i= i+1
@@ -58,6 +65,6 @@ def main():
         return 
     img = Image(sys.argv[1])
     H = HOG(img, 3, 16)
-
+    print H
 if __name__ == "__main__":
     main()
